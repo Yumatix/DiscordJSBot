b0f01a0e3c63720af9775881a70feef54d9d4acf
Kristin <kristinheggs@outlook.com>
2019-03-29 09:56:25 -0700

    Added greet message on new guild join.

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ extend(false, global.config, JSON.parse(fs.readFileSync("package.json")));
 const client = new Discord.Client();
 client.queues = [];
 
+//When bot connects to Discord's servers
 client.on("ready", () => {
     console.log(`Bot initiated. Type '?' for command line commands, or type ${config.command_symbol}help in discord for bot commands.`);
 
@@ -25,13 +26,16 @@ client.on("ready", () => {
 
 });
 
+//When client detects a message
 client.on("message", (message) => {
+    //PMs from owner
     if (message.guild == null && message.author.id == config.owner_id){
         console.log("RECIEVED DM FROM OWNER");
         message.author.sendMessage("<Generic owner response>");
         return;
     }
 
+    //Guild messages
     if (message.content.substring(0,1) == config.command_symbol){
         var substrings = message.content.split(" ");
         var command = substrings[0].substring(1);
@@ -44,7 +48,11 @@ client.on("message", (message) => {
     }
 });
 
+client.on("guildCreate", (guild) => {
+    playNewServerMessage(guild);
+});
 
+//When a command is passed via CLI
 readline.on("line", (line) => {
     var substrings = line.split(" ");
     var command = substrings[0];
@@ -67,6 +75,9 @@ readline.on("line", (line) => {
     else console.log(`Unknown console command: ${command}`);
 });
 
+// -- INIT -- 
+
+//Load keys from file, generate keys file if non-existant
 try {
     if (fs.existsSync("./keys.json")){ loadKeys(); }
     else throw error;
@@ -75,12 +86,14 @@ try {
     fs.writeFileSync("./keys.json", `{\n    "bot_token" : "",\n    "google_api_key" : ""\n}`);
 }
 
+//Check for bot token, provide prompt if missing, shutdown bot.
 if (!config.bot_token || config.bot_token == ""){
     console.log("ERROR: Bot token missing. Please add your bot token to keys.json. For more information, check out the CielBot documentation.\nREMINDER: Keep your bot token secret at all times! Never give it to anyone, or they will have access to your bot!");
     client.destroy();
     process.exit();
 }
 
+//Connect to Discord servers
 console.log(`Initiating bot..\nCommand Prefix: ${config.command_symbol}`);
 client.login(config.bot_token);
 
@@ -89,4 +102,33 @@ function loadKeys(){
     extend(false, global.config, JSON.parse(fs.readFileSync("./keys.json")));
 }
 
+//Plays when the bot joins a new server for the first time
+function playNewServerMessage(guild){
+
+    var ownerName = "";
+    var embed = {};
+    client.fetchUser(config.owner_id).then(user => {
+
+        ownerName = user.username;
+        
+        embed.color = 0x7936ca;
+        embed.footer = {"text":`CielBot ${config.version}`};
+        embed.fields = [
+            {"name":"Greetings!", 
+            "value":`Hi, ${guild.name}! I'm Ciel, a bot for Discord with plenty of features and abilites. If you'd like to learn more about what I can do, type \`/help\` into the chat at any time. If you'd like to learn about me and my creator, feel free to type \`/info\`! By the way, \`${ownerName}\` is in charge of me! My code is publicly available on Github if you'd like to be in charge of your very own copy of me.`},
+            {"name": "Note:", 
+            "value":`At this time, I'm still in alpha stage development. Many planned features aren't yet available. As of now, I'm able to function as a basic music bot. I can stream music from Youtube when given either a link or a search term. But my creator has lots more planned for my future! Keep an eye out, I'll let you know if I'm ever updated by \`${ownerName}\`.`},
+            {"name":"~~Dear server owner,~~ *Still under development... sorry.*", 
+            "value":`~~I come with some configurability options available to you, to help customize how I work in your server! Want to restrict my messages to a specific channel? Want me to automatically delete or keep certain messages? Need a few server management utilites to help keep things smooth? Type \`/config\` in any text channel on your server, and I'll send you a DM so we can set things up! During setup, you can even set other members or roles who can also manage me.~~`}
+        ];
+        
+
+        guild.channels.sort((c1, c2) => {
+            if(c1.type!==`text`) return 1;
+            if(!c1.permissionsFor(guild.me).has(`SEND_MESSAGES`)) return -1;
+            return c1.position < c2.position ? -1 : 1;
+        }).first().send({embed: embed});
+    });
+
+}
 
